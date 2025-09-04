bce585b4aa1ced3f5fd435e2075ba8bd9678485f
escandon100 <olakunle23odunade@gmail.com>
2025-09-04 11:08:23 +0100

    update

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ mongoose.connect(MONGO_URI)
   });
 
 
-app.use(cors({ origin : "http://localhost:5173" , credentials : true}));
+app.use(cors({ origin : "https://www.vixafx.com" , credentials : true}));
 app.use(express.json());
 
 app.use('/api/adminLogin', adminLoginRoutes);
